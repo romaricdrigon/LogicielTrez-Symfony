b6b3e5f2dc6a9a2850993325eea05fe9a42e47de
Romaric Drigon <romaric.drigon@gmail.com>
2012-10-07 16:17:50 +0200

    App structure

---
+++ src/Trez/LogicielTrezBundle/Controller/DefaultController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Trez\LogicielTrezBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+
+class DefaultController extends Controller
+{
+    public function indexAction($name)
+    {
+        return $this->render('TrezLogicielTrezBundle:Default:index.html.twig', array('name' => $name));
+    }
+}
---
+++ src/Trez/LogicielTrezBundle/TrezLogicielTrezBundle.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace Trez\LogicielTrezBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class TrezLogicielTrezBundle extends Bundle
+{
+}

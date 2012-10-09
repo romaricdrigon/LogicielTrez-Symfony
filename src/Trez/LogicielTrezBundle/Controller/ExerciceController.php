98491248063a24bd5b1d1c34981823da1bde1323
Romaric Drigon <romaric.drigon@gmail.com>
2012-10-09 10:16:30 +0200

    Crud operations ok with Exercice

--- src/Trez/LogicielTrezBundle/Controller/ExerciceController.php
+++ src/Trez/LogicielTrezBundle/Controller/ExerciceController.php
@@ -24,14 +24,14 @@ class ExerciceController extends Controller
 
     public function addAction()
     {
-        $proto = new Exercice();
-        $form = $this->get('form.factory')->create(new ExerciceType(), $proto);
+        $object = new Exercice();
+        $form = $this->get('form.factory')->create(new ExerciceType(), $object);
 
         if ('POST' === $this->get('request')->getMethod()) {
             $form->bindRequest($this->get('request'));
 
             if ($form->isValid()) {
-                $this->get('doctrine.orm.entity_manager')->persist($proto);
+                $this->get('doctrine.orm.entity_manager')->persist($object);
                 $this->get('doctrine.orm.entity_manager')->flush();
 
                 //$this->get('session')->setFlash('success', 'La source de données a été créée avec succès');
@@ -43,5 +43,40 @@ class ExerciceController extends Controller
         return $this->render('TrezLogicielTrezBundle:Exercice:add.html.twig', array(
             'form' => $form->createView()
         ));
+    }
+
+    public function editAction($id)
+    {
+        $em = $this->getDoctrine()->getEntityManager();
+        $object = $em->getRepository('TrezLogicielTrezBundle:Exercice')->find($id);
+        $form = $this->get('form.factory')->create(new ExerciceType(), $object);
+
+        if ('POST' === $this->get('request')->getMethod()) {
+            $form->bindRequest($this->get('request'));
+            if ($form->isValid()) {
+                $em->flush();
+
+                //$this->get('session')->setFlash('info', 'Vos modifications ont été enregistrées');
+
+                return new RedirectResponse($this->generateUrl('exercice_index'));
+            }
+        }
+
+        return $this->render('TrezLogicielTrezBundle:Exercice:edit.html.twig', array(
+            'form' => $form->createView(),
+            'exercice' => $object
+        ));
+    }
+
+    public function deleteAction($id)
+    {
+        $em = $this->get('doctrine.orm.entity_manager');
+        $object = $em->getRepository('TrezLogicielTrezBundle:Exercice')->find($id);
+        $em->remove($object);
+        $em->flush();
+
+        //$this->get('session')->setFlash('info', 'Vos modifications ont été enregistrées');
+
+        return new RedirectResponse($this->generateUrl('exercice_index'));
     }
 }

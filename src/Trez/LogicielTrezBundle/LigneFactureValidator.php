3788ba4940fc493ca90f363ba97fb073befdb0a8
Romaric Drigon <romaric.drigon@gmail.com>
2012-12-15 22:32:35 +0100

    Removed some deprecated code

--- src/Trez/LogicielTrezBundle/LigneFactureValidator.php
+++ src/Trez/LogicielTrezBundle/LigneFactureValidator.php
@@ -9,18 +9,10 @@ class LigneFactureValidator
 {
     public function isTotalUnder(Facture $facture, ExecutionContext $context)
     {
-        //$em = $this->get('doctrine.orm.entity_manager');
-
         $ligne_total = $facture->getLigneTotal();
 
-        /*$ligne = $facture->getLigne();
-        $ligne = $em->getRepository('TrezLogicielTrezBundle:Ligne')->find($ligne_id);
-        $factures = $em->getRepository('TrezLogicielTrezBundle:Facture')->findBy(
-            ['ligne' => $ligne],
-            ['numero' => 'DESC']);*/
-
         if ($facture->getMontant() > $ligne_total) {
-            $context->addViolationAtSubPath('montant', 'Le montant ne peut pas dépasser de la ligne, soit %ligne_total% € au maximum', ['%ligne_total%' => $ligne_total], null);
+            $context->addViolationAtSubPath('montant', 'Le montant ne peut pas dépasser de la ligne, soit %ligne_total% € au maximum', array('%ligne_total%' => $ligne_total), null);
         }
     }
 }

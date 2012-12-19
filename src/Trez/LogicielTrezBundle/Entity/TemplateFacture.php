48ba0a6b4f6e476a47a62561767a7a74aa21811f
crobert <cedric.robert@insa-lyon.fr>
2012-12-19 13:42:58 +0100

    ajout d'une entity TemplateFacture

---
+++ src/Trez/LogicielTrezBundle/Entity/TemplateFacture.php
@@ -0,0 +1,83 @@
+<?php
+
+namespace Trez\LogicielTrezBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * TemplateFacture
+ */
+class TemplateFacture
+{
+    /**
+     * @var integer
+     */
+    private $id;
+
+    /**
+     * @var string
+     */
+    private $name;
+
+    /**
+     * @var string
+     */
+    private $contenu;
+
+
+    /**
+     * Get id
+     *
+     * @return integer 
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set name
+     *
+     * @param string $name
+     * @return TemplateFacture
+     */
+    public function setName($name)
+    {
+        $this->name = $name;
+    
+        return $this;
+    }
+
+    /**
+     * Get name
+     *
+     * @return string 
+     */
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    /**
+     * Set contenu
+     *
+     * @param string $contenu
+     * @return TemplateFacture
+     */
+    public function setContenu($contenu)
+    {
+        $this->contenu = $contenu;
+    
+        return $this;
+    }
+
+    /**
+     * Get contenu
+     *
+     * @return string 
+     */
+    public function getContenu()
+    {
+        return $this->contenu;
+    }
+}

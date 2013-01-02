6892c37831132c565353093ee0d6c52ff086e69c
Romaric Drigon <romaric.drigon@gmail.com>
2013-01-02 19:17:26 +0100

    Fixed forgotten import in Tiers entity

--- src/Trez/LogicielTrezBundle/Entity/Tiers.php
+++ src/Trez/LogicielTrezBundle/Entity/Tiers.php
@@ -4,6 +4,7 @@ namespace Trez\LogicielTrezBundle\Entity;
 
 use Doctrine\ORM\Mapping as ORM;
 use Symfony\Component\Validator\Constraints as Assert;
+use Symfony\Bridge\Doctrine\Validator\Constraints\UniqueEntity;
 
 /**
  * Trez\LogicielTrezBundle\Entity\Tiers

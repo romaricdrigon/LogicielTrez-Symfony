da2f0e571e5cb901b66ae6d87d5fbda1d2464b85
Romaric Drigon <romaric.drigon@gmail.com>
2013-01-02 22:50:12 +0100

    Added a nice login page for credentials auth

--- src/Trez/LogicielTrezBundle/Controller/SecurityController.php
+++ src/Trez/LogicielTrezBundle/Controller/SecurityController.php
@@ -20,6 +20,24 @@ class SecurityController extends Controller
             $this->get('session')->setFlash('error', "L'authentification a échoué !");
         }
 
-        return $this->render('TrezLogicielTrezBundle:Security:login.html.twig', array());
+        return $this->render('TrezLogicielTrezBundle:Security:login_openid.html.twig', array());
+    }
+
+    public function loginCredentialsAction()
+    {
+        $request = $this->container->get('request');
+        $session = $request->getSession();
+
+        // get the error if any (works with forward and redirect -- see below)
+        if ($request->attributes->has(SecurityContext::AUTHENTICATION_ERROR)) {
+            $this->get('session')->setFlash('error', "Nom d'utilisateur ou mot de passe invalide");
+        } elseif (null !== $session && $session->has(SecurityContext::AUTHENTICATION_ERROR)) {
+            $session->remove(SecurityContext::AUTHENTICATION_ERROR);
+            $this->get('session')->setFlash('error', "Nom d'utilisateur ou mot de passe invalide");
+        }
+
+        return $this->render('TrezLogicielTrezBundle:Security:login_credentials.html.twig', array(
+            'last_username' => $session->get(SecurityContext::LAST_USERNAME)
+        ));
     }
 }

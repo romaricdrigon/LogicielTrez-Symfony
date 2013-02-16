7318c82c8acb4ff7d8134efc28a40b5a920bc8b5
Romaric Drigon <romaric.drigon@gmail.com>
2013-02-16 22:26:26 +0100

    AclProxy: fixes on eager fetch mode

--- src/Trez/LogicielTrezBundle/Service/AclProxy/FetchStrategy/EagerFetchStrategy.php
+++ src/Trez/LogicielTrezBundle/Service/AclProxy/FetchStrategy/EagerFetchStrategy.php
@@ -8,16 +8,17 @@ class EagerFetchStrategy extends AbstractFetchStrategy
 {
     public function fetchChildren($entity, $child_name)
     {
-        $children = $entity->{'get'.$child_name.'s'};
+        $children = $entity->{'get'.$child_name.'s'}();
 
         $validChildren = array();
 
         foreach ($children as $child) {
             $builderName = 'Trez\LogicielTrezBundle\Service\AclProxy\Builder\\'.$child_name.'Builder';
             $builder = new $builderName($this, $child);
-            $builder->build();
+            $builder->buildChildren();
 
             if ($builder->isValid() === true) {
+                $builder->buildTotaux();
                 $validChildren[] = $builder->getResult();
             }
         }

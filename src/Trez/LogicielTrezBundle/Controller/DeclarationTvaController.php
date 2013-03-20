3b647a1ada8b110ed25a89086a48948a7de25d67
Romaric Drigon <romaric.drigon@gmail.com>
2013-03-20 22:21:42 +0100

    Added DeclarationTva controller

---
+++ src/Trez/LogicielTrezBundle/Controller/DeclarationTvaController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Trez\LogicielTrezBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Response;
+
+class DeclarationTvaController extends Controller
+{
+    public function indexAction()
+    {
+        return new Response();
+    }
+}

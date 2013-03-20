7552912f393967e47ad5b8f69d3d3cff3e295ac8
Romaric Drigon <romaric.drigon@gmail.com>
2013-03-20 22:40:35 +0100

    Added skeleton for DeclarationTva controller

--- src/Trez/LogicielTrezBundle/Controller/DeclarationTvaController.php
+++ src/Trez/LogicielTrezBundle/Controller/DeclarationTvaController.php
@@ -9,6 +9,32 @@ class DeclarationTvaController extends Controller
 {
     public function indexAction()
     {
+        $defaultData = array('message' => 'Je ne sais pas à quoi cela sert');
+        $form = $this->createFormBuilder($defaultData)
+            ->add('mois', 'date')
+            ->getForm();
+
+        return $this->render('TrezLogicielTrezBundle:DeclarationTva:list.html.twig', array(
+                'form' => $form
+            ));
+    }
+
+    public function listFacturesAction($date)
+    {
+        if ($date == -1) {
+
+        }
+
+        return new Response();
+    }
+
+    public function createAction()
+    {
+        return new Response();
+    }
+
+    public function generateSheetAction()
+    {
         return new Response();
     }
 }

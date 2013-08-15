056a46b78c3bd5e8a8627dde09d84d890fec2ccb
Romaric Drigon <romaric.drigon@gmail.com>
2013-08-15 23:09:11 +0200

    ExceptionController is now a service, with DIC

--- src/Trez/LogicielTrezBundle/Controller/ExceptionController.php
+++ src/Trez/LogicielTrezBundle/Controller/ExceptionController.php
@@ -3,11 +3,14 @@
 namespace Trez\LogicielTrezBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Templating\TemplateReference;
+use Symfony\Component\HttpFoundation\Session\Session;
 use Symfony\Component\HttpKernel\Exception\FlattenException;
 use Symfony\Component\HttpKernel\Log\DebugLoggerInterface;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Bundle\TwigBundle\Controller\ExceptionController as OriginalExceptionController;
+use Symfony\Component\HttpFoundation\RedirectResponse;
+use Symfony\Component\Security\Core\SecurityContext;
 
 /**
  * Based on Symfony2 ExceptionController,
@@ -15,22 +18,43 @@ use Symfony\Bundle\TwigBundle\Controller\ExceptionController as OriginalExceptio
  */
 class ExceptionController extends OriginalExceptionController
 {
+    /**
+     * @var Session
+     */
+    protected $session;
+
+    /**
+     * @var \Symfony\Component\HttpFoundation\Request
+     */
+    protected $request;
+
+    /**
+     * @var \Symfony\Component\Security\Core\SecurityContext
+     */
+    protected $securityContext;
+
+    public function __construct(\Twig_Environment $twig, $debug, Session $session, Request $request, SecurityContext $sc)
+    {
+        parent::__construct($twig, $debug);
+
+        $this->session = $session;
+        $this->request = $request;
+        $this->securityContext = $sc;
+    }
+
     public function showAction(Request $request, FlattenException $exception, DebugLoggerInterface $logger = null, $format = 'html')
     {
         if ('Trez\LogicielTrezBundle\Exception\LockedException' === $exception->getClass()) {
-            $this->container->get('session')->getFlashBag()->set('error', "Vous ne pouvez pas éditer un exercice/budget verrouillé ou ses fils");
+            $this->session->getFlashBag()->set('error', "Vous ne pouvez pas éditer un exercice/budget verrouillé ou ses fils");
 
-            header('Location: '.$this->container->get('request')->getRequestUri());
-            exit();
+            return new RedirectResponse($this->request->getRequestUri(), 302);
         }
 
         if ('Symfony\Component\HttpKernel\Exception\AccessDeniedHttpException' === $exception->getClass()) {
-            $this->container->get('session')->getFlashBag()->set('error', "Vous n'avez pas les privilèges nécessaires pour effectuer cette action !");
+            $this->session->getFlashBag()->set('error', "Vous n'avez pas les privilèges nécessaires pour effectuer cette action !");
 
-            $sc = $this->container->get('security.context');
-
-            if ($sc->isGranted('ROLE_USER') === true) {
-                return new Response($this->container->get('templating')->render(
+            if ($this->securityContext->isGranted('ROLE_USER') === true) {
+                return new Response($this->twig->render(
                     'TrezLogicielTrezBundle:Default:index.html.twig',
                     array()
                 ));
